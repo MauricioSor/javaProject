8e3ee800746882679060ffac47e7b8d91f4fd1c4
Mauricio Soria <mauricioutn2017@gmail.com>
2024-10-27 19:29:17 -0300

    eliminacion de tests

--- src/main/java/com/v1/consultorio/ConsultorioApplication.java
+++ src/main/java/com/v1/consultorio/ConsultorioApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.v1.consultorio"})
 @SpringBootApplication
 
 public class ConsultorioApplication {
